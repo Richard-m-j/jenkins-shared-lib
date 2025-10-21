7eab20f34f0a22f806ea0fddb7f6153c9f813372
Richard-m-j <richardj815165@gmail.com>
2025-10-21 16:03:50 +0530

    fix: single cleanup command

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,7 +1,6 @@
 def call(String username, String projectName, String ImageTag){
     
     sh """
-     docker rmi ${username}/${projectName}:${ImageTag}
-     docker rmi ${username}/${projectName} ${username}/${projectName}:latest
+        docker rmi ${username}/${projectName}:${ImageTag} ${username}/${projectName}:latest
     """
 }
